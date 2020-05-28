fab0e1013fb55f89b1c43ef2b97ecd5912b174d9
Kellie <35340387+kellieshep@users.noreply.github.com>
2020-05-28 15:21:26 -0600

    my first commit

--- todos/todos.js
+++ todos/todos.js
@@ -1,8 +1,31 @@
 let toDo = [];
+let nextid = 1000;
+// Click on a close button to hide the current list item
+const close = document.getElementsByClassName("close");
+
+if(typeof(localStorage.toDoList) !== "undefined" && localStorage.toDoList != ""){
+    toDo = JSON.parse(localStorage.toDoList);
+    //console.log(toDo);
+
+    //toDo.forEach()
+    //alert(toDo.length);
+
+    let countStop = toDo.length;
+    for(let i = 0; i<countStop; i++){
+        //console.log(toDo[i]);
+        let item = toDo[i];
+        console.log(item);
+        console.log("hi");
+        buildItem(item.item, item.id, item.done, true);
+
+    }
+
+    //localStorage.toDoList = [];
+}
 
 const mylist = document.getElementsByTagName("li");
 let i;
-let nextid = 1000;
+
 for (i = 0; i < mylist.length; i++) {
     const span = document.createElement("SPAN");
     const txt = document.createTextNode("\u00D7");
@@ -10,8 +33,7 @@ for (i = 0; i < mylist.length; i++) {
     span.appendChild(txt);
     mylist[i].appendChild(span);
 }
-// Click on a close button to hide the current list item
-const close = document.getElementsByClassName("close");
+
 
 for (i = 0; i < close.length; i++) {
     close[i].onclick = function() {
@@ -23,63 +45,95 @@ for (i = 0; i < close.length; i++) {
 
 const list = document.querySelector('UL');
 list.addEventListener('click', function(ev) {
-    if (ev.target.tagName === 'LI') {
+    if (ev.target.tagName = 'LI') {
         ev.target.classList.toggle('checked');
         for (const toDoElement of toDo) {
-            if(toDoElement.id === ev.target.id){
+            if(toDoElement.id == ev.target.id){
                 toDoElement.done=!toDoElement.done;
+                //console.log(toDo);
             }
+
         }
     }
 }, false);
+
+//Creating a new list item and adding it to local storage.
+
 function newItem() {
-    const li = document.createElement("li");
+
     const inputValue = document.getElementById("newItem").value;
+    buildItem(inputValue);
+// stringify to JSON and adding it to local storage.
+    let toDoJson = JSON.stringify(toDo)
+    localStorage.toDoList = toDoJson;
+    console.log(toDoJson);
+
+}
+//Building the item from local storage and the new list items added.
+
+function buildItem(inputValue, id=1, done=null, initialBuild = false) {
+    const li = document.createElement("li");
     const t = document.createTextNode(inputValue);
+    //localStorage.setItem(li, inputValue);
     li.appendChild(t);
+    // Making sure that there is an item on the list
     if (inputValue === '') {
         alert("You must write something!");
     } else {
         document.getElementById("myList").appendChild(li);
     }
+    //building the list with the entered information.
     document.getElementById("newItem").value = "";
 
     const span = document.createElement("SPAN");
+    //adding the x to be able to remove the item.
     const txt = document.createTextNode("\u00D7");
     span.className = "close";
 
     span.appendChild(txt);
     li.appendChild(span);
-    li.id = nextid;
-    let newList = {id:nextid, item:inputValue, done:false};
-    nextid++;
-    toDo.push(newList);
-
-    for (i = 0; i < close.length; i++) {
-        close[i].onclick = function() {
-            const div = this.parentElement;
-            div.style.display = "none";
-        }
+    if (id !== null) {
+        li.id = id;
+    } else {
+        // adding a date as the id.
+        li.id = new Date().getTime();
+    }
+// adding done to the object
+    if (done !== null) {
+        li.done = done;
+    } else {
+        li.done = false;
     }
 
-//function saveList(){
-  //  let itemName = document.getElementById("inputValue").value
-   // let itemHTML = document.getElementById("newList").value
-   // localStorage.setItem(itemName, itemHTML)
-}
-// UI actions
-// Show a list of tasks
-// Add a new task
-// Complete a task
-// Remove a task
-// Filter tasks (complete/incomplete)
-//Data sources: localStorage
-//todo: { id : timestamp, content: string, completed: bool }
-//toDoList = [toDo];
-//Initial Module list: ToDos.js, localStorage helpers (ls.js), utilities.js
+    let newList = {id: new Date().getTime(), item: inputValue, done: li.done};
+    nextid++;
+    //adding the elements to the array.
+    if (!initialBuild) {
+        toDo.push(newList);
+    }
 
-//let leftToDo = (myList) => (document.getElementById("myNodeList").innerHTML=myList.getElementsByClassName("checked").length);
-//function myFunction() {
-//     var x = document.getElementsByClassName("checked").classList.length;
-//   document.getElementsByClassName("totals").innerHTML = x;
-// }
+// If the x is pressed to delete item this hides the item. **needs to delete the object from the array and updates storage as well.
+    for (let i = 0; i < close.length; i++) {
+        close[i].onclick = function () {
+            const div = this.parentElement;
+            div.remove();
+
+            for (const toDoElement of toDo) {
+                if (toDoElement.id = ev.target.id) {
+
+
+                }
+
+
+            }
+        }
+    }
+}
+//***Adding the total to the bottom of the list.
+function checkDone(done) {
+    return done == true;
+}
+
+function totals() {
+    document.getElementById("result").innerHTML = toDo.some(checkDone);
+}
